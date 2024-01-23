6142b648f10944aa69f49d038de62166d0ab6624
ArnanK <arnank9@gmail.com>
2024-01-23 11:02:01 -0500

    pq

--- Cracking The Coding Interview/Data Structures/PriorityQueue.py
+++ Cracking The Coding Interview/Data Structures/PriorityQueue.py
@@ -1,3 +1,4 @@
+# The above class implements a priority queue data structure using an array-based heap.
 class Empty(Exception):
     pass
 
@@ -7,7 +8,6 @@ class PriorityQueue:
         self._n = 0
         self._A = [None] * self._capacity
         
-
     def insert(self, value):
         self._n += 1
         if (self._n) >= self._capacity:
@@ -15,13 +15,13 @@ class PriorityQueue:
             self._resizeArray(self._capacity)
         self._A[self._n] = value
         self._percolateUp(self._n)
-        
 
     def deleteMin(self):
-        if self._n == 1:
-            raise Empty("There are no values within the Priority Queue")
+        if self._n == 0:
+            return 
         value = self._A[1]
         self._A[1] = self._A[self._n]
+        self._A[self._n] = None
         self._n -= 1
         self._percolateDown(1)
         return value
@@ -32,11 +32,19 @@ class PriorityQueue:
             self._A[index] = self._A[(index ) // 2]
             index = (index ) // 2
         self._A[index] = value
-
     
     def _percolateDown(self, index):
-        pass
-
+        value = self._A[index]
+        while index*2 <= self._n:
+            if self._A[index*2] < value and (index*2 == self._n or self._A[index*2] < self._A[(index*2)+1]):
+                self._A[index] = self._A[index*2]
+                index = index * 2
+            elif ((index*2)+1) <= self._n and self._A[(index*2)+1] < value:
+                self._A[index] = self._A[(index*2)+1]
+                index = (index * 2) + 1
+            else:
+                break
+        self._A[index] = value
 
     def _resizeArray(self, newCapacity):
         b = [None] * newCapacity
@@ -53,5 +61,11 @@ p.insert(43)
 p.insert(2)
 p.insert(20)
 p.insert(3)
+print(p.deleteMin())
+print(p.deleteMin())
+print(p.deleteMin())
+
+
+
 
 print(p._A)
